63e766a03c6bf4a4c336d0341068889c4e86b821
essingen123 <essingen123@gmail.com>
2024-08-07 08:32:06 +0200

    committi pitti

---
+++ A_0_🚀💻🎉🦄✨🌟💖💫🌈_shortcut_alias-ish_2_A_1.py
@@ -0,0 +1,8 @@
+# 🌟✨🦄🎉 Welcome to my awesome code! 🎉🦄✨🌟
+# 🌈💫💖 Made with love and magic 💖💫🌈
+# 🚀💻 Let's code something amazing! 💻🚀
+
+import A_1_run_me_main_start
+from A_1_run_me_main_start import main_x
+
+main_x()
---
+++ A_1_run_me_main_start.py
@@ -0,0 +1,10 @@
+# 🌟✨🦄🎉 Welcome to my awesome code! 🎉🦄✨🌟
+# 🌈💫💖 Made with love and magic 💖💫🌈
+# 🚀💻 Let's code something amazing! 💻🚀
+
+from ba0_local_server_port_seeker import start_server_local
+
+def main_x():
+    start_server_local()
+
+main_x()
